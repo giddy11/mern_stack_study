152651a4b322104676ef3c751803571ddc7f1703
giddy11 <edoghotugiddy@gmail.com>
2024-12-04 15:17:18 +0100

    updated

---
+++ week_2/index.js
@@ -0,0 +1,13 @@
+/**
+ * Types of Selectors:
+ * Type/element
+ * class selectors
+ * Id selectors
+ * Universal selectors
+ * Attribute selectors
+ * Descendant selectors
+ * 
+ * Read about Child selectors, Adjascent selectors, general siblings selectors
+ * 
+ * 
+ */
