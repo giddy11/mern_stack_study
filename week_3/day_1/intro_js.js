088dfaca9a144a1fae18e3fc41408cf44589810a
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2024-12-11 16:45:59 +0100

    updated

---
+++ week_3/day_1/intro_js.js
@@ -0,0 +1,29 @@
+/**
+ * Importance of Javascript:
+ * 
+ * 1. Data and Information management (CRUD)
+ * 2. Logic and operational procedures
+ * 3. External communication
+ * 4. Realtime monitoring and management
+ * 5. Dynamic Rendering
+ * 6. Routing
+ * 
+ * Types of information that can be stored within JS
+ * Text/String - 
+ * Numbers
+ * Booleans
+ * Objects
+ * Null
+ * Undefined
+ * 
+ * firstname- lowercase
+ * last_name- snake case
+ * FirstName- Pascal case
+ * middleName- camel case
+ */
+
+first = "Emmanuel";
+last = "Pinnick";
+full_name = first + last;
+
+console.log(full_name)
