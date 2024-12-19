b94f3021f165a62a7c8e55f5dae817a50e030bbb
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2024-12-19 15:46:12 +0100

    updated

---
+++ week_4/day_2/script.js
@@ -0,0 +1,14 @@
+function handleEvent() {
+    console.log("Event from HTML");
+    // alert("Hello")
+}
+
+function mouseEnterEvent() {
+    console.log("Mouse entered Div item");
+    alert("Mouse entered Div item")
+}
+
+function addNumbers(a,b) {
+    let res = a + b;
+    console.log(res)
+}
