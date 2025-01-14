f097297d6fccb40666403143a5fd21cfc4720276
Emmanuel Pinnick <emmanuelpinnick@gmail.com>
2025-01-14 22:37:02 +0000

    Started section head component

--- week_5/my_app/src/components/section_head.js
+++ week_5/my_app/src/components/section_head.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import "../styles/section_head.css"
 
 export default function Section_head() {
   return (
     <div>
-         <h2 className='section_name'>TEAM</h2>
-            <div className='white_div'><span className='blue_span'></span></div>
-            <p className='section_text'>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        <div className='head_container'>
+            <h2 className='section_name'>TEAM</h2>
+            <div className='white_div'></div>
+            <p className='section_text'>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velitk</p>
+        </div>
     </div>
   )
 }
