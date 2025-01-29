d9d4a74dd8d0d5e7568102ed3c093f32d8b03bb2
Gideon Edoghotu <edoghotugiddy@gmail.com>
2025-01-29 18:21:41 +0100

    updated

--- noteapp/backend/db/db.js
+++ noteapp/backend/db/db.js
@@ -1,19 +1,13 @@
 import mongoose from "mongoose";
 
-const connectToMongoDB =  () => {
-    mongoose.connect("mongodb+srv://EDOGHOTU:GigiCarly%401@cluster0.z7bzhhy.mongodb.net/noteapp")
-    console.log("Connected to MongoDB")
-
-}
-
-// const connectToMongoDB = async () => {
-//   try {
-//     await mongoose.connect(
-//       "mongodb+srv://EDOGHOTU:GigiCarly%401@cluster0.z7bzhhy.mongodb.net/noteapp"
-//     );
-//   } catch (error) {
-//     console.log("Error connecting to MongoDB", error.message);
-//   }
-// };
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://EDOGHOTU:GigiCarly%401@cluster0.z7bzhhy.mongodb.net/noteapp"
+    );
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
+};
 
 export default connectToMongoDB;
--- noteapp/backend/routes/auth.js
+++ noteapp/backend/routes/auth.js
@@ -7,72 +7,52 @@ console.log(User);
 
 const router = express.Router();
 
-// router.post('/register', async  (req, res) => {
-//     try {
-//         const {name, email, password} = req.body;
-//         const user = await User.findOne({email})
-//         if (user) {
-//             return res.status(401).json({success: false, message: "User already exist"})
-//         }
+router.post('/register', async  (req, res) => {
+    try {
+        const {name, email, password} = req.body;
+        const user = await User.findOne({email})
+        if (user) {
+            return res.status(401).json({success: false, message: "User already exist"})
+        }
 
-//         const hashPassword = await bcrypt.hash(password, 10)
+        const hashPassword = await bcrypt.hash(password, 10)
 
-//         const newUser = new User({
-//             name, email, password: hashPassword
-//         })
+        const newUser = new User({
+            name, email, password: hashPassword
+        })
 
-//         await newUser.save()
+        await newUser.save()
 
-//         return res.status(200).json({success:true, message: "Account Created Successfully"})
-//     } catch (error) {
-//         return res.status(500).json({success: false, message: "Error in Adding User"})
-//     }
-// })
-
-router.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const user = User.findOne({ email });
-  if (user) {
-    return res
-      .status(401)
-      .json({ success: false, message: "User already exist" });
-  }
-
-  const hashPassword = bcrypt.hash(password, 10);
-
-  const newUser = new User({
-    name,
-    email,
-    password: hashPassword,
-  });
-
-  newUser.save();
-
-  return res
-    .status(200)
-    .json({ success: true, message: "Account Created Successfully" });
-});
-
-router.post("/register", (req, res) => {
-    const { email, password } = req.body;
-    const user = User.findOne({ email });
-    if (!user) {
-      return res
-        .status(401)
-        .json({ success: false, message: "User Not exist" });
+        return res.status(200).json({success:true, message: "Account Created Successfully"})
+    } catch (error) {
+        return res.status(500).json({success: false, message: "Error in Adding User"})
     }
+})
 
-    const checkPassword = bcrypt.compare(password, user.password)
-
-    if(!checkPassword) {
-        return res.status(401).json({success: false, message: "Wrong Credentials"})
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+          return res
+            .status(401)
+            .json({ success: false, message: "User Not exist" });
+        }
+    
+        const checkPassword = await bcrypt.compare(password, user.password)
+    
+        if(!checkPassword) {
+            return res.status(401).json({success: false, message: "Wrong Credentials"})
+        }
+    
+        const token = jwt.sign({id: user._id}, "secretkeyofnoteapp123@#", {expiresIn: "5h"})
+      
+        return res
+          .status(200)
+          .json({ success: true, token, user: {name: user.name}, message: "Login Successfully"});
+    } catch (error) {
+        return res.status(500).json({success: false, message: "Error in Logging Server"})
     }
-
-    const token = jwt.sign({id: user._id}, "secretkeyofnoteapp123@#", {expiresIn: "5h"})
-  
-    return res
-      .status(200)
-      .json({ success: true, token, user: {name: user.name}, message: "Login Successfully"});
   });
 
 export default router;
